26ef6d1ca22c60484b21f80a2838b18b9c158cad
Ankit Sharma <sankit72@gmail.com>
2021-12-27 21:21:08 +0530

    updated integrated the logout behaviour

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import logger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import userReducer from "./reducers/userSlice";
@@ -24,4 +24,6 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>(); // Export a hook that can be reused to resolve types
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 export default store;
