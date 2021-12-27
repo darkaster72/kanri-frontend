83237e5318f494dab6751eb6aa02e9ba82456ad7
Ankit Sharma <sankit72@gmail.com>
2021-12-27 21:21:08 +0530

    updated integrated the logout behaviour

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,47 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { IUser } from "../modes/user";
+import { selectCurrentUser } from "../redux/reducers/userSelector";
+import { logout } from "../redux/reducers/userSlice";
+import { AppDispatch, useAppDispatch, useAppSelector } from "../redux/store";
 import Logo from "./Logo";
 
 type AppProps = {};
 
-const Header = ({}: AppProps) => (
-  <nav className="fixed w-full">
-    <div className="container mx-auto flex justify-between px-10 py-3 align-baseline">
-      <div className="flex items-center">
-        <Logo></Logo>
-        <Link to="/">
-          <h3 className="text-4xl px-3 py-2 font-bold text-black">Kanri</h3>
-        </Link>
+const Header = ({}: AppProps) => {
+  const user = useAppSelector(selectCurrentUser);
+  const dispatch = useAppDispatch();
+
+  return (
+    <nav className="fixed w-full">
+      <div className="container mx-auto flex justify-between px-10 py-3 align-baseline">
+        <div className="flex items-center">
+          <Logo></Logo>
+          <Link to="/">
+            <h3 className="text-4xl px-3 py-2 font-bold text-black">Kanri</h3>
+          </Link>
+        </div>
+        {user ? renderLoggedInUser(user, dispatch) : renderLoginOptions()}
       </div>
-      <div className="flex no-underline items-center gap-2">
-        <Link to="login">
-          <Button>Login</Button>
-        </Link>
-        <Link to="sign-up">
-          <Button variant="outlined"> Sign Up</Button>
-        </Link>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Header;
+const renderLoginOptions = () => (
+  <div className="flex no-underline items-center gap-2">
+    <Link to="login">
+      <Button>Login</Button>
+    </Link>
+    <Link to="sign-up">
+      <Button variant="outlined"> Sign Up</Button>
+    </Link>
+  </div>
+);
+
+const renderLoggedInUser = (user: IUser, dispatch: AppDispatch) => (
+  <div className="flex items-center">
+    <div>{user.email}</div>
+    <Button onClick={() => dispatch(logout())}>Log out</Button>
+  </div>
+);
